e25bee2455327f913f0f47e7f54466ed3328d6db
remopas <remopas@hotmail.nl>
2021-04-01 14:20:48 +0200

    registered discover module

--- src/store/discover.module.js
+++ src/store/discover.module.js
@@ -1,13 +1,30 @@
 
+import util from '@/util.js'
+
 const discoverModule = {
   state: () => ({
-    locations: []
+    locations: [],
+    userLocation: undefined,
+    selectedLocationId: undefined
   }),
   mutations: {
-
+    locations: (state) => {
+      if(state.userLocation){
+        return state.locations.map(l => {
+          l.distance = util.haversine(l.coords.latitude, l.coords.longitude, state.userLocation.coords.latitude, state.userLocation.coords.longitude)
+          return l
+        })
+      }else{
+        return state.locations
+      }
+    },
+    userLocation: (state) => state.userLocation,
+    selectedLocationId: (state) => state.selectedLocationId,
+    selectedLocation: (state) => state.locations.find(l => l.id === state.selectedLocationId)
   },
   getters: {
-
+    setUserLocation: (state, location) => state.userLocation = location,
+    setSelectedLocationId: (state, id) => state.selectedLocationId = id
   }
 }
 
