e03e16d4bc9418b703ba52f618c8b50e8fdd8d32
remopas <remopas@hotmail.nl>
2021-04-01 15:04:17 +0200

    added vuex action

---
+++ src/api/locations.endpoint.js
@@ -0,0 +1,15 @@
+
+import axios from 'axios'
+
+export default {
+
+  getLocations(query = undefined){
+    console.log(query)
+    return new Promise((resolve, reject) => 
+      axios.get('localhost:3000/api/v1/locations')
+      .then(res => resolve(res.data.locations))
+      .catch(err => reject(err))
+    )
+  }
+
+}
--- src/store/discover.module.js
+++ src/store/discover.module.js
@@ -1,5 +1,6 @@
 
 import util from '@/util.js'
+import locationsAPI from '@/api/locations.endpoint.js'
 
 export default {
   namespaced: true,
@@ -10,10 +11,11 @@ export default {
   }),
   mutations: {
     setUserLocation: (state, location) => state.userLocation = location,
+    setLocations: (state, locations) => state.locations = locations,
     setSelectedLocationId: (state, id) => state.selectedLocationId = id
   },
   getters: {
-    locations: (state) => {
+    locations(state){
       if(state.userLocation){
         return state.locations.map(l => {
           l.distance = util.haversine(l.coords.latitude, l.coords.longitude, state.userLocation.coords.latitude, state.userLocation.coords.longitude)
@@ -26,6 +28,17 @@ export default {
     userLocation: (state) => state.userLocation,
     selectedLocationId: (state) => state.selectedLocationId,
     selectedLocation: (state) => state.locations.find(l => l.id === state.selectedLocationId)
+  },
+  actions: {
+    fetchLocations({commit}, query){
+      return new Promise((resolve, reject) => locationsAPI.getLocations(query)
+        .then(locations => {
+          commit('discover/setLocations', locations)
+          resolve()
+        })
+        .catch(err => reject(err))
+      ) 
+    }
   }
 }
 
