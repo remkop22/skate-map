4fc4a63f5bc61e48dd45653e3f5fd605dcbbb150
remopas <remopas@hotmail.nl>
2021-04-07 22:04:16 +0200

    changed dev-port back to 3000

--- vue.config.js
+++ vue.config.js
@@ -2,7 +2,7 @@ module.exports = {
   devServer: {
     proxy: {
       '^/api': {
-        target: 'http://localhost:8080',
+        target: 'http://localhost:3000',
         changeOrigin: true
       }
     }
