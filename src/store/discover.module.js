06b8e93b27722638024947bba191a5a757df4eb1
remopas <remopas@hotmail.nl>
2021-04-12 17:01:12 +0200

    added fetchers for objects and tags

--- src/store/discover.module.js
+++ src/store/discover.module.js
@@ -7,6 +7,8 @@ export default {
 
   state: () => ({
     locations: [],
+    tags: [],
+    objects: [],
     userLocation: undefined,
     selectedLocationId: undefined
   }),
@@ -14,7 +16,9 @@ export default {
   mutations: {
     setUserLocation: (state, location) => state.userLocation = location,
     setLocations: (state, locations) => state.locations = locations,
-    setSelectedLocationId: (state, id) => state.selectedLocationId = id
+    setSelectedLocationId: (state, id) => state.selectedLocationId = id,
+    setTags: (state, tags) => state.tags = tags,
+    setObjects: (state, objects) => state.objects = objects
   },
   
   getters: {
@@ -22,15 +26,21 @@ export default {
       l.distance = state.userLocation ? util.haversine(l.coords, state.userLocation.coords) : undefined
       return l
     }),
+    tags: (state) => state.tags,
+    objects: (state) => state.objects,
     userLocation: (state) => state.userLocation,
     selectedLocationId: (state) => state.selectedLocationId,
     selectedLocation: (state) => state.locations.find(l => l.id === state.selectedLocationId)
   },
   
   actions: {
+
     fetchLocations: async ({commit, state}, {query, filters, sort}) => 
       locationsAPI.getLocations(query, sort, filters, state.userLocation)
         .then(locations => commit('setLocations', locations)),
+
+    fetchObjects: async ({commit}) => locationsAPI.getObjects().then(objects => commit('setObjects', objects)),
+    fetchTags: async ({commit}) => locationsAPI.getTags().then(tags => commit('setTags', tags))
   }
 }
 
